f10a24a5fc1e30fcd9db02fec07065541c79e81c
ZeEE <874074406@qq.com>
2024-06-25 15:40:54 +0800

    Initial Commit

---
+++ harmonyNEXT/harmonyNEXTApp.swift
@@ -0,0 +1,17 @@
+//
+//  harmonyNEXTApp.swift
+//  harmonyNEXT
+//
+//  Created by 何金泽 on 2024/6/25.
+//
+
+import SwiftUI
+
+@main
+struct harmonyNEXTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
